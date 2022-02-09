ee12fb1a80a61549a1e4243975a6e343151783b5
Levchishin.L <58588278+banana-party@users.noreply.github.com>
2022-02-09 20:51:30 +0300

    удалены неймспейсы

--- DXTest/Services/CsvReadService.cs
+++ DXTest/Services/CsvReadService.cs
@@ -2,12 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Media;
 
 namespace DXTest.Services
 {
